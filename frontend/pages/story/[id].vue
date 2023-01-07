ccaa8f0a65f6fb6b8599522046edb15874c5191a
quocvinh0401 <lequocvinh140213@gmail.com>
2023-01-07 12:19:03 +0700

    update

--- frontend/pages/story/[id].vue
+++ frontend/pages/story/[id].vue
@@ -44,6 +44,11 @@
                                         <icon name="ion:trash-outline" size="20"/>
                                         <span>Delete photo</span>
                                     </button>
+                                    <div class="absolute right-0 top-0">
+                                        <svg height="12" viewBox="0 0 21 12" width="21" fill="white" style="transform: scale(-1, -1) translate(0px, 6px);">
+                                            <path d="M20.685.12c-2.229.424-4.278 1.914-6.181 3.403L5.4 10.94c-2.026 2.291-5.434.62-5.4-2.648V.12h20.684z"></path>
+                                        </svg>
+                                    </div>
                                 </div>
                             </div>
                         </div>
